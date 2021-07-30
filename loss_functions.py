1ef84b45811c8f6b6aa13d52f5e813d97786eef4
ndgnuh <ndgnuh@protonmail.com>
2021-07-30 20:09:06 +0700

    Loss functions

--- loss_functions.py
+++ loss_functions.py
@@ -1,42 +1,116 @@
+import torch
+import torch.nn.functional as F
 import torch.nn as nn
 import torchvision.ops as visionops
-# C Error on import
-# from pytorch_metric_learning import losses as pml_loss
-# TODO: add custom loss functions
-# - TripletLoss (available in torch?)
-# - MultiTask Loss
-# - ArcLoss
 
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
 
-def is_loss_function(name: str):
-    if name not in nn.modules.loss.__dict__:
-        return False
-    c = nn.modules.loss.__dict__[name]
-    if type(c) == type and issubclass(c, nn.modules.loss._Loss):
-        return True
-    return False
+    def forward(self, inputs, targets, smooth=1):
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return 1 - dice
 
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
 
-def find_loss_function(name: str):
-    if name in nn.__dict__ and is_loss_function(name):
-        return nn.__dict__[name]()
-    if name in visionops.__dict__ and is_loss_function(name):
-        return visionops.__dict__[name]
+    def forward(self, inputs, targets, smooth=1):
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+        
+        return Dice_BCE
 
+class IoULoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoULoss, self).__init__()
 
-def all_loss_functions():
-    all = {}
-    for lf in nn.modules.loss._Loss.__subclasses__():
-        origname = lf.__name__
-        name = lf.__name__
-        if not name.startswith('_'):
-            for i in range(10):
-                if name not in all:
-                    break
-                name = f"{origname}_{i}"
-            all[name] = lf
-    return all
+    def forward(self, inputs, targets, smooth=1):
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return 1 - IoU
 
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalLoss, self).__init__()
 
-class ArcLoss(nn.modules.loss._Loss):
-    pass
+    def forward(self, inputs, targets, alpha=0.8, gamma=2, smooth=1):
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #first compute binary cross-entropy 
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        BCE_EXP = torch.exp(-BCE)
+        focal_loss = alpha * (1-BCE_EXP)**gamma * BCE
+                       
+        return focal_loss
+
+ALPHA = 0.5
+BETA = 0.5
+GAMMA = 1
+
+class FocalTverskyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalTverskyLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=ALPHA, beta=BETA, gamma=GAMMA):
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        #True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()    
+        FP = ((1-targets) * inputs).sum()
+        FN = (targets * (1-inputs)).sum()
+
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        FocalTversky = (1 - Tversky)**gamma
+
+        return FocalTversky
+
+ALPHA = 0.5 # < 0.5 penalises FP more, > 0.5 penalises FN more
+CE_RATIO = 0.5 #weighted contribution of modified CE loss compared to Dice loss
+class ComboLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(ComboLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=ALPHA, beta=BETA, eps=1e-9):
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        intersection = (inputs * targets).sum()    
+        dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        
+        inputs = torch.clamp(inputs, eps, 1.0 - eps)       
+        out = - (ALPHA * ((targets * torch.log(inputs)) + ((1 - ALPHA) * (1.0 - targets) * torch.log(1.0 - inputs))))
+        weighted_ce = out.mean(-1)
+        combo = (CE_RATIO * weighted_ce) - ((1 - CE_RATIO) * dice)
+        
+        return combo
